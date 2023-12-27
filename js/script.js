9af897c4135841d42d5cd23293db38af6bcdb30e
Juan Sebastián Frásica G <juansebastianfrasica@hotmail.com>
2023-12-27 11:12:23 -0500

    Agregando mapa de Google Maps

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 if (document.getElementById('my-work-link')) {
   document.getElementById('my-work-link').addEventListener('click', () => {
-    document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
+    document.getElementById('my-work-section').scrollIntoView({ behavior: "smooth" })
   })
 }
